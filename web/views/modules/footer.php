9be764880bbdb70d9dbb394a69dd3a03e3f8ff4d
Samuel Humberto Moreno Rojas <samuel.moreno.isw@unipolidgo.edu.mx>
2025-03-11 21:20:04 -0600

    11 Marzo

--- web/views/modules/footer.php
+++ web/views/modules/footer.php
@@ -1,3 +1,217 @@
+<style>
+    a:link,
+    a:visited,
+    a:hover,
+    a:active {
+        text-decoration: none !important;
+        color: inherit !important;
+
+    }
+</style>
+
+
+<div class="container-fluid bg-dark small">
+
+    <div class="container py-5 text-light">
+
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-2">
+
+            <div class="col row">
+
+                <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+
+                    <h4 class="lead">
+
+                        <a href="#" class="text-uppercase">Productos</a>
+                        <hr class="border-white">
+
+                        <ul>
+                            <li href="#">
+                                Gaming
+                            </li>
+
+                            <li href="#">
+                                Punto de Venta
+                            </li>
+
+                            <li href="#">
+                                Computo
+                            </li>
+
+                            <li href="#">
+                                Redes
+                            </li>
+
+                            <li href="#">
+                                Ensambles de Equipos a Medida
+                            </li>
+
+                            <li href="#">
+                                Servidores
+                            </li>
+
+                            <li href="#">
+                                Papeleria
+                            </li>
+
+                            <li href="#">
+                                Consumibles
+                            </li>
+
+                            <li href="#">
+                                Impresoras
+                            </li>
+
+                            <li href="#">
+                                Proyectores
+                            </li>
+
+                            <li href="#">
+                                Energia
+                            </li>
+
+                            <li href="#">
+                                Accesorios
+                            </li>
+
+                            <li href="#">
+                                Adaptadores y cables
+                            </li>
+
+                            <li href="#">
+                                Domotica
+                            </li>
+
+                        </ul>
+
+                    </h4>
+
+                </div>
+
+                <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+
+                    <h4 class="lead">
+
+                        <a href="#" class="text-uppercase">Servicios</a>
+                        <hr class="border-white">
+
+                        <ul>
+                            <li href="#">
+                                Audio y Video
+                            </li>
+
+                            <li href="#">
+                                Videovigilancia
+                            </li>
+
+                            <li href="#">
+                                Instalacion de Redes
+                            </li>
+
+                            <li href="#">
+                                Punto de Venta
+                            </li>
+
+                            <li href="#">
+                                Edificios Inteligentes
+                            </li>
+
+                            <li href="#">
+                                Arrendamiento de Equipos
+                            </li>
+
+                            <li href="#">
+                                Aulas Inteligentes
+                            </li>
+
+                            <li href="#">
+                                Centro de Servicio
+                            </li>
+
+                            <li href="#">
+                                Robotica Educativa
+                            </li>
+
+                        </ul>
+
+                    </h4>
+
+                </div>
+
+
+                <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+
+                    <h4 class="lead">
+
+                        <a href="#" class="text-uppercase">Soluciones</a>
+                        <hr class="border-white">
+
+                        <ul>
+                            <li href="#">
+                                Gobierno
+                            </li>
+
+                            <li href="#">
+                                Iniciativa privada
+                            </li>
+
+                            <li href="#">
+                                Educacion
+                            </li>
+
+                            <li href="#">
+                                Gaming
+                            </li>
+
+                            <li href="#">
+                                Hogas y Oficina
+                            </li>
+
+
+                        </ul>
+
+                    </h4>
+
+                </div>
+
+
+            </div>
+
+            <div class="col my-3 my-lg-0 px-lg-5 text-light">
+
+                <h1 class="lead">Dudas y comentarios, contactenos aqui:</h1>
+                <br>
+
+                <h1 class="lead"><i class="fa fa-phone-square pe-2"></i> +52(618)555-55-55
+
+                    <br><br>
+
+                    <i class="fa fa-envelope pe-2"></i> tuputamadre@gmail.com
+
+                    <br><br>
+
+                    <i class="fa fa-map-marker pe-2"></i> Calle tu puta madre, esquina con tu madre
+
+                    <br><br>
+
+                    Durango, Durango.
+                </h1>
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3644.149421027966!
+                    2d-104.65511672471128!3d24.025795478483523!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.
+                    1!3m3!1m2!1s0x869bc82733cf53cd%3A0x2c7ec3737c993471!2sPacs%20Computaci%C3%B3n%20S
+                    .A.%20de%20C.V.!5e0!3m2!1ses-419!2smx!4v1741725309278!5m2!1ses-419!2smx" width="100%" 
+                    height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
+
 <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
